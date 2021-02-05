302c7065f6a94593a2e312500d0f6ae8ff56edfb
Tv <Anand_Tv@Dell.com>
2021-02-05 09:58:53 +0530

    first commit

---
+++ src/main/java/com/example/sbdemo/TestEx.java
@@ -0,0 +1,14 @@
+package com.example.sbdemo;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TestEx {
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+}
---
+++ src/test/java/com/example/sbdemo/SbdemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.sbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//@Test
+class SbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
